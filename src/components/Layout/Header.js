3537e0ce46313a90ff342f8100d273f6e7d3a967
RajMazing <caseyraj92@gmail.com>
2022-07-07 23:04:52 -0500

    Modal now closes when Backdrop is clicked or the Close button is clicked.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,8 +8,8 @@ import classes from './Header.module.css'
 const Header = props => {
   return <>
   <header className={classes.header} > 
-    <h1> ReactMeals</h1>
-    <HeaderCartButton />
+    <h1>F00d1E</h1>
+    <HeaderCartButton onClick={props.onShowCart} />
     </header>
     <div className={classes['main-image']}>
       <img src={mealsImg} alt="A table full of Food" />
