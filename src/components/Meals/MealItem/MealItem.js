2dc6ec5430c0937e8f614a3e43ed2e7b9fea20c7
RajMazing <caseyraj92@gmail.com>
2022-07-08 23:56:40 -0500

    working with refs, carticon badge now renders items added to cart. cart now adds items when clicked

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,26 @@
 
+import { useContext } from 'react'
+
+
 import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css'
+import CartContext from '../../../store/cart-context'
 
 
 const MealItem = props => {
+  const  cartCtx = useContext(CartContext);
 
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+  }
+  
   return <li className={classes.meal}>
     <div> <h3>
       {props.name}
@@ -14,7 +29,7 @@ const MealItem = props => {
       <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm  id={props.id} onAddToCart={addToCartHandler} />
       </div>
      </li>
 }
