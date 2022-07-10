6cceb1e62b7c8f62faad05ccc0a3b408a369e316
RajMazing <caseyraj92@gmail.com>
2022-07-09 22:27:32 -0500

    outputting cart items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,37 +1,36 @@
 
-import { useContext } from 'react'
+import { useContext } from 'react';
 
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
-import MealItemForm from './MealItemForm'
-import classes from './MealItem.module.css'
-import CartContext from '../../../store/cart-context'
-
-
-const MealItem = props => {
-  const  cartCtx = useContext(CartContext);
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
-    })
-  }
-  
-  return <li className={classes.meal}>
-    <div> <h3>
-      {props.name}
-      </h3>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
+      price: props.price,
+    });
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm  id={props.id} onAddToCart={addToCartHandler} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
-     </li>
-}
+    </li>
+  );
+};
 
 export default MealItem;
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -41,7 +41,7 @@ const MealItemForm = (props) => {
   }}/>
 
   <button> + Add </button>
-  {!amountIsValid && <p> Please enter avalid amount (1-5).</p>}
+  {!amountIsValid && <p> Please enter a valid amount (1-5).</p>}
   </form>
   );
 }
